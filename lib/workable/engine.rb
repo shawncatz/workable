74b19f773c5397db2f4febd2793005dd541d2d85
Shawn Catanzarite <me@shawncatz.com>
2014-09-13 18:49:54 -0700

    force engine to load sidekiq dependencies, they don't seem to load properly otherwise (failures not visible in ui)

--- lib/workable/engine.rb
+++ lib/workable/engine.rb
@@ -4,5 +4,11 @@ module Workable
     config.autoload_paths << File.expand_path("../lib", __FILE__)
     config.autoload_paths << File.expand_path("../app/workable", __FILE__)
     config.autoload_paths << File.expand_path("app/workable", __FILE__)
+
+    initializer 'workable_sidekiq' do
+      require 'sidekiq'
+      require 'sidekiq/scheduler'
+      require 'sidekiq/failures'
+    end
   end
 end
