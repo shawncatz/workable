dbe8098aea7705adcf92af069182ed23b9a15d1d
Shawn Catanzarite <me@shawncatz.com>
2014-09-23 23:54:30 -0700

    bug in module loading order

--- lib/workable/sidekiq.rb
+++ lib/workable/sidekiq.rb
@@ -1,5 +1,3 @@
-require File.expand_path('../sidekiq/middleware', __FILE__)
-require File.expand_path('../sidekiq/handler', __FILE__)
 
 module Workable::Sidekiq
   class << self
@@ -8,3 +6,6 @@ module Workable::Sidekiq
     end
   end
 end
+
+require File.expand_path('../sidekiq/middleware', __FILE__)
+require File.expand_path('../sidekiq/handler', __FILE__)
